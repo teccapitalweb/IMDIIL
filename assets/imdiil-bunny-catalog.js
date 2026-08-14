ff646a5fccf932418b8249d195802aa1822d4bad
TEC Capital web <teccapitalweb@gmail.com>
2026-08-14 17:06:36 -0600

    Add files via upload

---
+++ assets/imdiil-bunny-catalog.js
@@ -0,0 +1,417 @@
+// Generado desde imdiil-bunny-upload-state.json. No contiene claves privadas.
+window.IMDIIL_BUNNY_CATALOG = [
+  {
+    "titulo": "Controller JR Avanzado Finanzas, Gestión y Reporting Estratégico",
+    "collectionId": "299238b8-bb2f-4f05-9a77-c5e306e4479d",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "0ed58f60-9a75-40dc-add4-45d843f043ee"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "c441dfa0-d986-40ce-a634-a0449adb73b6"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "3e975053-3624-4430-8c8b-51000ea98f00"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "3f62e15b-49aa-4fd0-861d-3559a0c5739b"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "f37fcf2b-afb2-44f9-bc60-3f195e48036b"
+      }
+    ]
+  },
+  {
+    "titulo": "Forecasting Maestro Domina la Demanda",
+    "collectionId": "dc0a4bfa-98fe-484f-8046-1e0d8f6896ac",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "881ac107-9218-4791-a2d4-a40e6c32a41f"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "91a1e830-af62-46c6-970c-d0f85aaa22bb"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "45dd08b5-53bc-4105-b6ca-a613143d8518"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "c97de124-6042-4db8-a401-cbe9b6d4b4d1"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "4bb20dfb-ee4a-4f9c-916e-fdda0458f881"
+      }
+    ]
+  },
+  {
+    "titulo": "Gestión de Inventario y Buenas Prácticas de Almacén",
+    "collectionId": "ef1ccdd1-5bc0-4621-aeb2-e86362f2b692",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "d4a22969-6fd6-44b5-abe3-7de00a797c8a"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "f334140d-381a-49d0-a784-23fcdb5deec3"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "31703021-fc0e-4c9a-9edc-5656cb9d6b0b"
+      },
+      {
+        "numero": 4,
+        "titulo": "Sesión 4",
+        "provider": "bunny",
+        "videoId": "06f39263-f1fb-42d6-8bd0-a5d6443395f0"
+      },
+      {
+        "numero": 5,
+        "titulo": "Sesión 5",
+        "provider": "bunny",
+        "videoId": "0bed0b13-dc4d-472b-8ae8-9dda35ee8bbb"
+      }
+    ]
+  },
+  {
+    "titulo": "Ingeniería de Procesos Lean, Six Sigma y Mejora Continua",
+    "collectionId": "68e742ef-9a67-4022-92a5-949927eebb24",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "8a929a73-f4cb-463c-bb93-8be425208b8e"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "788d489c-be9c-4a07-9b34-009beafd3793"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "38021779-02e4-4cc5-be9a-c8ac5940aa70"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "d760de75-42fd-4733-88bb-40011c39d138"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "eafeba68-ad67-46c3-89f0-a4fd2c1a5129"
+      }
+    ]
+  },
+  {
+    "titulo": "Kaizen Innovación Continua",
+    "collectionId": "d2a8f17e-aa80-49d9-864d-f537fdd9d1a9",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "82c7f560-7166-48d7-8d0a-18fc6405dc94"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "1fe00d78-d491-4732-8d4f-f2dd15a6b347"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "fcd31aef-8673-418f-a849-4ab08bbac5b1"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "b5b092d9-33de-40d3-9e0d-636cd4e775c2"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "8dcba572-6bb1-4ea9-805f-df36de73389b"
+      }
+    ]
+  },
+  {
+    "titulo": "Lean Manufacturing 4.0 y Mejora Continua",
+    "collectionId": "9df26dba-95da-44f9-849d-ad092fc49289",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "0acb0c4c-71d8-4b4a-a123-6082ac0c298a"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "170f6a5f-7b96-4c62-9d38-8491827a3a8c"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "0850c66d-0f33-4f69-a803-28a7bf4a1355"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "e792e35f-c13e-4e9e-b5d0-119c17746083"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "92e16922-a4dd-4a0b-b351-f1797535bbaa"
+      }
+    ]
+  },
+  {
+    "titulo": "Normatividad y Regulación de Energías Renovables",
+    "collectionId": "b467784e-7b76-4a2f-ab47-752abb631fa8",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1 y 2",
+        "provider": "bunny",
+        "videoId": "ff8ba9f6-0643-476f-8b2c-a8b3589e88b4"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 3 y 4",
+        "provider": "bunny",
+        "videoId": "4c344243-58e8-4a3b-801c-cbc307377cbe"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "52783e9a-2b82-42f3-abb1-22a6028c47e2"
+      }
+    ]
+  },
+  {
+    "titulo": "Planificación y Control de Costos",
+    "collectionId": "0c163dc3-a7a5-4475-8232-56714523ba15",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "b76b26c8-f662-4c1b-b027-cb6c944cd37c"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "83fe46a2-f924-4b62-94ed-cb6c9ac01d07"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "4d49eb03-643d-495f-9cef-d6e750da5e8a"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "405e4d7a-dd3e-4011-ba8b-c52fef9c6f5e"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "80476286-87ee-4bfd-9290-0eedd5396e05"
+      }
+    ]
+  },
+  {
+    "titulo": "Project Management",
+    "collectionId": "b3375194-7f38-4a8b-be5f-c2b2330812c4",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "5fa9b57b-2800-4dff-aff8-7dcc21a57c27"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "8597d3a5-f9f7-4fd5-8699-444c327ce9e7"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "d0bce5fc-4ced-4bc8-b0b7-962ff165ca00"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "74263391-a7d3-4840-8b05-49a810ec74c1"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "e3150b26-38fa-4a95-ad45-dc5e70a9bdf1"
+      }
+    ]
+  },
+  {
+    "titulo": "Secretariado Administrativo Oferta y Demanda Actual",
+    "collectionId": "4fe811c7-8fe8-424e-bdbf-7d0fa5d89f21",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "4410df42-32dd-4f25-9ee8-6e58a801d9ed"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "5012c40b-c7af-49a8-aec6-e45741a56d7e"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "81bc1e55-3aa2-40e0-b36c-cf965c6e6313"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "d6161ae4-f8c2-4ded-add3-c5de2afb2fe8"
+      }
+    ]
+  },
+  {
+    "titulo": "Supervisión de Producción (Nivel Avanzado)",
+    "collectionId": "e214aa5e-380a-4530-83df-96c9534b7692",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "c02fe26b-938d-433a-82be-f6f306979b27"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "9a3db66a-cd8e-4b9f-9dbf-28a1110dba53"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "2b1c4c0f-6c30-4a4a-89a5-e2912bbec773"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "bdc86f8a-9945-4f05-b51b-7ff27d2432b8"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "17e99c55-0c25-4485-a894-0160b3a911f3"
+      }
+    ]
+  },
+  {
+    "titulo": "Tratamiento Biológico de Aguas Residuales",
+    "collectionId": "c3976aa2-81a9-4c10-b7a9-950212e52418",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "30fb069f-2057-4f06-afbf-57cb36170906"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "7c798454-82ec-44be-9ccb-67276d170dfa"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "fdf34714-9f5a-4bb5-a1ec-4c82d06b05a7"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "748491bc-3edb-42ee-b604-673dfd83bfff"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "70a9461c-481b-4392-a6f6-911c87c3cebe"
+      }
+    ]
+  }
+];
